4dd36ab5d2c8c4eb27407acdd467586dc6337a5f
bengala vulgo trl <105220901+gbz77@users.noreply.github.com>
2024-11-22 08:26:05 -0300

    KKKKKKKKKKKKKKKKKKK

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,7 +1,9 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function VizualizarinformacoesGlobais(){
+async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
-    const dados = res.json()
-    console.log(dados)
+    const dados = await res.json()
+    console.log(dados);
 }
+
+vizualizarInformacoesGlobais()
